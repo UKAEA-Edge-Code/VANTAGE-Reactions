81f929e164a55feb797c78be96d8f886b985c44f
SMijin <stefan.mijin@ukaea.uk>
2024-02-15 16:34:03 +0000

    Fixed wrong cross_product bug

--- src/reactions_lib/utils.hpp
+++ src/reactions_lib/utils.hpp
@@ -22,7 +22,7 @@ std::vector<T> cross_product(const std::vector<T>& a,const std::vector<T>& b){
 
     result[0] = a[1]*b[2] - a[2]*b[1];
     result[1] = a[2]*b[0] - a[0]*b[2];
-    result[2] = a[0]*b[1] - a[1]*b[2];
+    result[2] = a[0]*b[1] - a[1]*b[0];
 
     return result;
 }
