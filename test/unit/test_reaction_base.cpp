bd131d75dcb043dd4da17d8890a71846bb210d01
SMijin <stefan.mijin@ukaea.uk>
2024-07-18 10:44:43 +0100

    Added AMJUEL 2D T,n data class with coronal limits

--- test/unit/test_reaction_base.cpp
+++ test/unit/test_reaction_base.cpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "common_markers.hpp"
-#include "common_transformations.hpp"
 #include "data_calculator.hpp"
 #include "mock_reactions.hpp"
 #include "reaction_base.hpp"
@@ -9,6 +8,7 @@
 #include <derived_reactions/electron_impact_ionisation.hpp>
 #include <gtest/gtest.h>
 #include <memory>
+#include <reaction_data/AMJUEL_2D_data.hpp>
 #include <reaction_data/fixed_coefficient_data.hpp>
 #include <reaction_data/fixed_rate_data.hpp>
 #include <transformation_wrapper.hpp>
@@ -121,6 +121,89 @@ TEST(ReactionData, FixedCoefficientData) {
 
     for (int rowx = 0; rowx < nrow; rowx++) {
       EXPECT_EQ(weight->at(rowx, 0), 0.64);
+    }
+  }
+
+  particle_group->domain->mesh->free();
+}
+TEST(ReactionData, AMJUEL2DData) {
+  const int N_total = 1000;
+
+  auto particle_group = create_test_particle_group(N_total);
+  auto particle_sub_group = std::make_shared<ParticleSubGroup>(particle_group);
+
+  auto particle_spec = particle_group->get_particle_spec();
+
+  auto amjuel_data = AMJUEL2DData<2, 2>(
+      3e12, 1.0, 1.0, 1.0,
+      std::array<std::array<REAL, 2>, 2>{std::array<REAL, 2>{1.0, 0.02},
+                                         std::array<REAL, 2>{0.01, 0.02}});
+
+  auto test_reaction =
+      LinearReactionBase<0, AMJUEL2DData<2, 2>, TestReactionKernels<0>>(
+          particle_group->sycl_target, Sym<REAL>("TOT_REACTION_RATE"), 0,
+          std::array<int, 0>{}, std::vector<ParticleProp<REAL>>{},
+          std::vector<ParticleProp<INT>>{}, amjuel_data,
+          TestReactionKernels<0>(), particle_spec);
+
+  int cell_count = particle_group->domain->mesh->get_cell_count();
+  auto descendant_particles = std::make_shared<ParticleGroup>(
+      particle_group->domain, particle_group->get_particle_spec(),
+      particle_group->sycl_target);
+
+  // Rate calculated based on ne=3e18, T=2eV, with the evolved quantity
+  // normalised to 3e12
+  auto expected_rate = 3.880728735562758;
+  for (int i = 0; i < cell_count; i++) {
+
+    test_reaction.run_rate_loop(particle_sub_group, i);
+    auto rate = particle_group->get_cell(Sym<REAL>("TOT_REACTION_RATE"), i);
+    const int nrow = rate->nrow;
+
+    for (int rowx = 0; rowx < nrow; rowx++) {
+      EXPECT_NEAR(rate->at(rowx, 0), expected_rate, 1e-15);
+    }
+  }
+
+  particle_group->domain->mesh->free();
+}
+TEST(ReactionData, AMJUEL2DData_coronal) {
+  const int N_total = 1000;
+
+  auto particle_group = create_test_particle_group(N_total);
+  auto particle_sub_group = std::make_shared<ParticleSubGroup>(particle_group);
+
+  auto particle_spec = particle_group->get_particle_spec();
+
+  //Manipulating the normalisation quantities to trigger the coronal limit calculation 
+  auto amjuel_data = AMJUEL2DData<2, 2>(
+      3e6, 1e-6, 1.0, 1.0,
+      std::array<std::array<REAL, 2>, 2>{std::array<REAL, 2>{1.0, 0.02},
+                                         std::array<REAL, 2>{0.01, 0.02}});
+
+  auto test_reaction =
+      LinearReactionBase<0, AMJUEL2DData<2, 2>, TestReactionKernels<0>>(
+          particle_group->sycl_target, Sym<REAL>("TOT_REACTION_RATE"), 0,
+          std::array<int, 0>{}, std::vector<ParticleProp<REAL>>{},
+          std::vector<ParticleProp<INT>>{}, amjuel_data,
+          TestReactionKernels<0>(), particle_spec);
+
+  int cell_count = particle_group->domain->mesh->get_cell_count();
+  auto descendant_particles = std::make_shared<ParticleGroup>(
+      particle_group->domain, particle_group->get_particle_spec(),
+      particle_group->sycl_target);
+
+  // Rate calculated based on ne=3e18, T=2eV, with the evolved quantity
+  // normalised to 3e6, and density normalisation set to trigger the coronal limit
+  auto expected_rate = 2.737188973785161;
+  for (int i = 0; i < cell_count; i++) {
+
+    test_reaction.run_rate_loop(particle_sub_group, i);
+    auto rate = particle_group->get_cell(Sym<REAL>("TOT_REACTION_RATE"), i);
+    const int nrow = rate->nrow;
+
+    for (int rowx = 0; rowx < nrow; rowx++) {
+      EXPECT_NEAR(rate->at(rowx, 0), expected_rate, 1e-15);
     }
   }
 
