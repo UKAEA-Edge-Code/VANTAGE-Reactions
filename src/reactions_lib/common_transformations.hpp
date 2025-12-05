5b600331e5b1632aec486e80fd8f81010557b93f
Stefan Mijin <stefan.mijin@ukaea.uk>
2025-12-05 11:11:40 +0000

    Added the CellwiseDistributor transform (#170)
    * Added the CellwiseDistributor transform
    
    * Update src/reactions_lib/common_transformations.hpp
    
    Co-authored-by: Will Saunders <77331509+will-saunders-ukaea@users.noreply.github.com>
    
    * Updated docstrings
    
    ---------
    
    Co-authored-by: Will Saunders <77331509+will-saunders-ukaea@users.noreply.github.com>

--- src/reactions_lib/common_transformations.hpp
+++ src/reactions_lib/common_transformations.hpp
@@ -158,7 +158,7 @@ template <typename T> struct CellwiseAccumulator : TransformationStrategy {
    * @brief Constructor for CellwiseAccumulator.
    *
    * @param template_group A template particle group used to provide the
-   * CellDatConsts for the dats specified by dat_names.
+   * ParticleDats specified by dat_names for the created CellDatConsts.
    * @param dat_names A vector of strings specifying the names of the dats
    * to be accumulated cell-wise.
    */
@@ -220,7 +220,7 @@ template <typename T> struct CellwiseAccumulator : TransformationStrategy {
    * assigned
    */
   void set_cell_data(std::string data_name,
-                     std::vector<CellData<T>> cell_data) {
+                     std::vector<CellData<T>> &cell_data) {
 
     NESOASSERT(this->values.find(Sym<T>(data_name)) != this->values.end(),
                "Attempted to retrieve values for " + data_name +
@@ -254,6 +254,135 @@ template <typename T> struct CellwiseAccumulator : TransformationStrategy {
 private:
   std::vector<Sym<T>> dats;
   std::map<Sym<T>, std::shared_ptr<CellDatConst<T>>> values;
+};
+
+/**
+ * @brief Transfomation strategy that accumulates distributes values of certain
+ * particle dats from provided cell-wise data
+ *
+ * @tparam T REAL or INT
+ */
+template <typename T> struct CellwiseDistributor : TransformationStrategy {
+
+  CellwiseDistributor() = delete;
+
+  /**
+   * @brief Constructor for CellwiseDistributor.
+   *
+   * @param template_group A template particle group used to provide the
+   * ParticleDats specified by dat_names for the created CellDatConsts.
+   * @param dat_names A vector of strings specifying the names of the dats
+   * to be distributed cell-wise.
+   */
+  CellwiseDistributor(ParticleGroupSharedPtr template_group,
+                      std::vector<std::string> dat_names) {
+
+    for (auto name : dat_names) {
+      NESOASSERT(
+          template_group->contains_dat(Sym<T>(name)),
+          "Particle dat " + name +
+              " not in passed template particle group in CellwiseDistributor");
+      this->dats.push_back(Sym<T>(name));
+    }
+
+    std::vector<INT> num_comps_vec;
+    for (auto &dat : dats) {
+      auto particle_dat = template_group->get_dat(dat);
+
+      num_comps_vec.push_back(particle_dat->ncomp);
+    }
+    this->comp_nums = std::make_shared<LocalArray<INT>>(
+        template_group->sycl_target, num_comps_vec);
+
+    for (auto i = 0; i < std::size(this->dats); i++) {
+      this->values.emplace(std::make_pair(
+          this->dats[i], std::make_shared<CellDatConst<T>>(
+                             template_group->sycl_target,
+                             template_group->domain->mesh->get_cell_count(),
+                             num_comps_vec[i], 1)));
+    }
+  }
+  /**
+   * @brief Distribute the dats registered in this transform.
+   *
+   * @param target_subgroup Subgroup containing particles among which the dats
+   * will be distributed.
+   */
+  void transform_v(ParticleSubGroupSharedPtr target_subgroup) override {
+
+    for (auto i = 0; i < this->dats.size(); i++) {
+      auto loop = particle_loop(
+          "distributor_loop", target_subgroup,
+          [=](auto var, auto comp_nums, auto buffer) {
+            for (auto j = 0; j < comp_nums.at(i); j++) {
+              var[j] = buffer.at(j, 0);
+            }
+          },
+          Access::write(this->dats[i]), Access::read(this->comp_nums),
+          Access::read(this->values[this->dats[i]]));
+
+      loop->execute();
+    }
+  }
+
+  /**
+   * @brief Extract the cell-wise data as a standard vector of
+   * CellData objects
+   *
+   * @param data_name Name of the particle dat to be extracted
+   */
+  std::vector<CellData<T>> get_cell_data(std::string data_name) {
+
+    NESOASSERT(this->values.find(Sym<T>(data_name)) != this->values.end(),
+               "Attempted to retrieve values for " + data_name +
+                   " which is not registered in the CellwiseDistributor");
+    return this->values[Sym<T>(data_name)]->get_all_cells();
+  }
+
+  /**
+   * @brief Sets cell-wise data from a standard vector of CellData
+   * objects
+   *
+   * @param data_name Name of the particle dat to be set
+   * @param cell_data Standard vector of CellData objects with data to be
+   * assigned
+   */
+  void set_cell_data(std::string data_name,
+                     std::vector<CellData<T>> &cell_data) {
+
+    NESOASSERT(this->values.find(Sym<T>(data_name)) != this->values.end(),
+               "Attempted to retrieve values for " + data_name +
+                   " which is not registered in the CellwiseDistributor");
+    this->values[Sym<T>(data_name)]->set_all_cells(cell_data);
+  }
+
+  /**
+   * @brief Zero out the distribution buffer for a given particle dat
+   *
+   * @param data_name Name of the dat whose associated buffer should be zeroed
+   * out
+   */
+  void zero_buffer(std::string data_name) {
+    NESOASSERT(this->values.find(Sym<T>(data_name)) != this->values.end(),
+               "Attempted to zero out buffer for " + data_name +
+                   " which is not registered in the CellwiseDistributor");
+    this->values[Sym<T>(data_name)]->fill(0);
+  }
+
+  /**
+   * @brief Zero out all buffers
+   */
+  void zero_all_buffers() {
+
+    for (auto name : this->dats) {
+      this->zero_buffer(name.name);
+    }
+  }
+
+private:
+  std::vector<Sym<T>> dats;
+  std::map<Sym<T>, std::shared_ptr<CellDatConst<T>>> values;
+  std::shared_ptr<LocalArray<INT>> comp_nums;
 };
 
 /**
