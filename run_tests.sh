458b248aa1ceb8194c650b827003f4c48796e58c
Sanket Gadgil <gadgil48@googlemail.com>
2026-01-16 15:36:51 +0000

    Switch to using lcov instead of gcovr

--- run_tests.sh
+++ run_tests.sh
@@ -5,5 +5,9 @@ spack env activate -p -d .
 spack install
 spack load reactions
 OMP_NUM_THREADS=1 mpirun -n 1 unit_tests
-mkdir coverage_report
-pipx run gcovr build-linux-ubuntu24.04*/*/test/unit/CMakeFiles/unit_tests.dir -r ./src --txt coverage_report/coverage.txt
+mkdir -p coverage
+find ./src -name '*.hpp' -exec cp -t ./coverage {} +
+find ./build-linux-ubuntu24.04*/*/test/unit/CMakeFiles/unit_tests.dir -name '*.gcno' -exec cp -t ./coverage {} +
+find ./build-linux-ubuntu24.04*/*/test/unit/CMakeFiles/unit_tests.dir -name '*.gcda' -exec cp -t ./coverage {} +
+find ./coverage -name '*.hpp' -exec gcov -pb {} \;
+lcov --ignore-errors mismatch -c -d ./coverage --output-file ./coverage/coverage.txt
