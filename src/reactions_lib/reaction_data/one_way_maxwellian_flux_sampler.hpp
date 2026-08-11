6600fcfe92cea659628505c947aaa2646f9db186
Omstavan Samant <39756220+Omstavan@users.noreply.github.com>
2026-08-11 15:15:24 +0100

    Truncated/positive Maxwellian sampler for surface recycling (#239)
    * added truncated_maxwellian_sampler
    
    * working test before removing templating
    
    * removed ndim templating
    
    * removed stray comments, unused maxwellian sampler
    
    * fix clang precommit checks
    
    * renamed truncated_maxwellian_sampler to one_way_maxwellian_flux_sampler, optimised cardano cubic solver function a bit,removed stray comment
    
    * added one-way sampler sphinx documentation

---
+++ src/reactions_lib/reaction_data/one_way_maxwellian_flux_sampler.hpp
@@ -0,0 +1,232 @@
+#ifndef REACTIONS_ONE_WAY_MAXWELLIAN_FLUX_SAMPLER_H
+#define REACTIONS_ONE_WAY_MAXWELLIAN_FLUX_SAMPLER_H
+#include "../particle_properties_map.hpp"
+#include "../reaction_data.hpp"
+#include "../utils.hpp"
+#include <iostream>
+#include <neso_particles.hpp>
+#include <vector>
+
+using namespace NESO::Particles;
+namespace VANTAGE::Reactions {
+/**
+ * @brief On device: Reaction data class for calculating velocity samples from a
+ * one way / truncated Maxwellian distribution given a fluid temperature and
+ * flow speed. The one way / truncated distribution is generated using a
+ * rejection sampling method outlined in
+ * https://doi.org/10.1088/0031-8949/90/1/015204.
+ *
+ */
+struct OneWayMaxwellianFluxOnDevice
+    : public ReactionDataBaseOnDevice<3, HostAtomicBlockKernelRNG<REAL>> {
+
+  OneWayMaxwellianFluxOnDevice() = default;
+
+  OneWayMaxwellianFluxOnDevice(const REAL &norm_ratio)
+      : norm_ratio(norm_ratio) {};
+
+  /**
+   * @brief solves the cubic equation used to find the maximum of
+   * the rejection sampling function for the one way / truncated maxwellian
+   * distribution
+   *
+   * @param d Ratio of flow speed to sigma (thermal spread) of the distribution
+   * @return maximum of the rejection sampling function
+   */
+  inline static REAL cardano_cubic_solver(REAL d) {
+    const REAL coef_a = 2.0;
+    const REAL coef_b = -4.0 - d;
+    const REAL coef_c = d;
+    const REAL coef_d = 1.0;
+    const REAL coef_a_sq = coef_a * coef_a;
+
+    const REAL p =
+        (3.0 * coef_a * coef_c - coef_b * coef_b) / (3.0 * coef_a_sq);
+    const REAL q =
+        (2.0 * coef_b * coef_b * coef_b - 9.0 * coef_a * coef_b * coef_c +
+         27.0 * coef_a_sq * coef_d) /
+        (27.0 * coef_a_sq * coef_a);
+
+    const REAL sqrt_term = Kernel::sqrt(-p / 3.0);
+    const REAL root =
+        2.0 * sqrt_term *
+        Kernel::cos(sycl::acos((3.0 * q) / (2.0 * p * sqrt_term)) / 3.0 -
+                    2.0 * REAL(M_PI) / 3.0);
+    return root - coef_b / (3.0 * coef_a);
+  }
+
+  /**
+   * @brief Gives the unnormalized function used in the one way / truncated
+   * maxwellian rejection sampling algorithm
+   *
+   * @param d Ratio of flow speed to sigma (thermal spread) of the distribution
+   * @return maximum of the rejection sampling function
+   */
+  inline static REAL rejection_function(REAL d, REAL t) {
+    const REAL s = 1.0 - t;
+    return t * Kernel::exp(-((t / s - d) * (t / s - d)) / 2.0) / (s * s * s);
+  }
+
+  REAL sample_positive_maxwellian(
+      REAL drift, REAL thermal_sigma, const Access::LoopIndex::Read &index,
+      typename HostAtomicBlockKernelRNG<REAL>::KernelType &kernel,
+      int &sample_counter, bool &is_kernel_valid) const {
+    if (!is_kernel_valid || thermal_sigma <= REAL(0.0)) {
+      return 0.0;
+    }
+
+    const REAL d = drift / thermal_sigma;
+    const REAL maxval = rejection_function(d, cardano_cubic_solver(d));
+    REAL candidate = 0.0;
+    REAL compare = 0.0;
+
+    do {
+      candidate = kernel.at(index, sample_counter++, &is_kernel_valid);
+      compare = maxval * kernel.at(index, sample_counter++, &is_kernel_valid);
+      if (!is_kernel_valid) {
+        return 0.0;
+      }
+
+      if (compare <= rejection_function(d, candidate)) {
+        return (candidate / (1.0 - candidate)) * thermal_sigma;
+      }
+    } while (true);
+  }
+
+  std::array<REAL, 3>
+  calc_data(const Access::LoopIndex::Read &index,
+            const Access::SymVector::Write<INT> &req_int_props,
+            const Access::SymVector::Read<REAL> &req_real_props,
+            typename HostAtomicBlockKernelRNG<REAL>::KernelType &kernel) const {
+    const REAL fluid_temperature_dat =
+        req_real_props.at(this->fluid_temperature_ind, index, 0);
+
+    std::array<REAL, 3> fluid_flow_speed{};
+    std::array<REAL, 3> basis_e1{};
+    std::array<REAL, 3> basis_e2{};
+    std::array<REAL, 3> basis_pi{};
+
+    for (int i = 0; i < 3; i++) {
+      fluid_flow_speed[i] =
+          req_real_props.at(this->fluid_flow_speed_ind, index, i);
+      basis_e1[i] = req_real_props.at(this->basis_e1_ind, index, i);
+      basis_e2[i] = req_real_props.at(this->basis_e2_ind, index, i);
+      basis_pi[i] = req_real_props.at(this->basis_pi_ind, index, i);
+    }
+
+    REAL drift_e1 = 0.0;
+    REAL drift_e2 = 0.0;
+    REAL drift_pi = 0.0;
+    for (int i = 0; i < 3; i++) {
+      drift_e1 += fluid_flow_speed[i] * basis_e1[i];
+      drift_e2 += fluid_flow_speed[i] * basis_e2[i];
+      drift_pi += fluid_flow_speed[i] * basis_pi[i];
+    }
+
+    const REAL thermal_sigma =
+        Kernel::sqrt(fluid_temperature_dat * this->norm_ratio);
+
+    bool is_kernel_valid = true;
+    int sample_counter = 0;
+
+    REAL rand1 = kernel.at(index, sample_counter++, &is_kernel_valid);
+    REAL rand2 = kernel.at(index, sample_counter++, &is_kernel_valid);
+
+    if (!is_kernel_valid) {
+      req_int_props.at(this->panic_ind, index, 0) += 1;
+      return std::array<REAL, 3>{};
+    }
+
+    auto normal_samples = utils::box_muller_transform(rand1, rand2);
+    const REAL sample_e1 = drift_e1 + thermal_sigma * normal_samples[0];
+    const REAL sample_e2 = drift_e2 + thermal_sigma * normal_samples[1];
+
+    const REAL sample_pi =
+        sample_positive_maxwellian(drift_pi, thermal_sigma, index, kernel,
+                                   sample_counter, is_kernel_valid);
+    if (!is_kernel_valid) {
+      req_int_props.at(this->panic_ind, index, 0) += 1;
+      return std::array<REAL, 3>{};
+    }
+
+    std::array<REAL, 3> sampled_vels{};
+    for (int i = 0; i < 3; i++) {
+      sampled_vels[i] = sample_e1 * basis_e1[i] + sample_e2 * basis_e2[i] +
+                        sample_pi * basis_pi[i];
+    }
+
+    return sampled_vels;
+  }
+
+public:
+  int fluid_flow_speed_ind, fluid_temperature_ind, basis_e1_ind, basis_e2_ind,
+      basis_pi_ind, panic_ind;
+  REAL norm_ratio;
+};
+
+/**
+ * @brief Reaction data class for sampling a velocity vector from a drifting
+ * Maxwellian in the tangential directions and a one way / truncated Maxwellian
+ * along the surface-normal direction.
+ * surface_basis_e1, surface_basis_e2, surface_basis_pi: The three basis
+ * vectors that define the surface tangential and normal directions.
+ * These are assumed to be orthonormal. See EIRENE docs section 1.5 Recycling
+ * surface sources for more details.
+ * @param norm_ratio The ratio of the temperature and kinetic energy
+ * normalisations. Specifically kT/mv^2 where m is the mass of the ions, and T
+ * and v are the temperature and velocity normalisation constants
+ * @param rng_kernel A shared pointer of a HostAtomicBlockKernelRNG<REAL> to
+ * be set as the rng_kernel in ReactionDataBase.
+ * @param properties_map (Optional) A std::map<int, std::string> object to be
+ * used when remapping property names.
+ */
+struct OneWayMaxwellianFluxSampler
+    : public ReactionDataBase<OneWayMaxwellianFluxOnDevice, 3,
+                              HostAtomicBlockKernelRNG<REAL>> {
+
+  constexpr static auto props = default_properties;
+
+  constexpr static auto required_simple_real_props = std::array<int, 5>{
+      props.fluid_flow_speed, props.fluid_temperature, props.surface_basis_e1,
+      props.surface_basis_e2, props.surface_basis_pi};
+
+  constexpr static auto required_simple_int_props =
+      std::array<int, 1>{props.panic};
+
+  OneWayMaxwellianFluxSampler(
+      const REAL &norm_ratio,
+      std::shared_ptr<HostAtomicBlockKernelRNG<REAL>> rng_kernel,
+      std::map<int, std::string> properties_map = get_default_map())
+      : ReactionDataBase<OneWayMaxwellianFluxOnDevice, 3,
+                         HostAtomicBlockKernelRNG<REAL>>(
+            Properties<INT>(required_simple_int_props),
+            Properties<REAL>(required_simple_real_props), properties_map) {
+    this->on_device_obj = OneWayMaxwellianFluxOnDevice(norm_ratio);
+    this->set_rng_kernel(rng_kernel);
+    this->index_on_device_object();
+  }
+
+  void index_on_device_object() {
+    this->on_device_obj->fluid_flow_speed_ind =
+        this->required_real_props.find_index(
+            this->properties_map.at(props.fluid_flow_speed));
+
+    this->on_device_obj->fluid_temperature_ind =
+        this->required_real_props.find_index(
+            this->properties_map.at(props.fluid_temperature));
+
+    this->on_device_obj->basis_e1_ind = this->required_real_props.find_index(
+        this->properties_map.at(props.surface_basis_e1));
+
+    this->on_device_obj->basis_e2_ind = this->required_real_props.find_index(
+        this->properties_map.at(props.surface_basis_e2));
+
+    this->on_device_obj->basis_pi_ind = this->required_real_props.find_index(
+        this->properties_map.at(props.surface_basis_pi));
+
+    this->on_device_obj->panic_ind = this->required_int_props.find_index(
+        this->properties_map.at(props.panic));
+  };
+};
+}; // namespace VANTAGE::Reactions
+#endif
---
+++ test/unit/test_one_way_maxwellian_flux_sampler.cpp
@@ -0,0 +1,111 @@
+#include "include/mock_particle_group.hpp"
+#include <gtest/gtest.h>
+
+#include "../../src/reactions/reactions.hpp"
+using namespace NESO::Particles;
+using namespace VANTAGE::Reactions;
+
+TEST(OneWayMaxwellianFluxSampler, SamplesExpectedVelocityWithDeterministicRNG) {
+
+  const int N_total = 100;
+  auto particle_group = create_test_particle_group<3>(N_total);
+
+  particle_group->add_particle_dat(Sym<REAL>("SURFACE_BASIS_E1"), 3);
+  particle_group->add_particle_dat(Sym<REAL>("SURFACE_BASIS_E2"), 3);
+  particle_group->add_particle_dat(Sym<REAL>("SURFACE_BASIS_PI"), 3);
+  particle_group->add_particle_dat(Sym<REAL>("SAMPLED_VELOCITY"), 3);
+
+  std::array<REAL, 3> flow_speed{};
+  std::array<REAL, 3> basis_e1{};
+  std::array<REAL, 3> basis_e2{};
+  std::array<REAL, 3> basis_pi{};
+  flow_speed[0] = -std::sqrt(2.0);
+  flow_speed[1] = std::sqrt(3.0);
+  flow_speed[2] = std::sqrt(5.0);
+  basis_e1[0] = 2.0 / std::sqrt(38.0);
+  basis_e1[1] = 3.0 / std::sqrt(38.0);
+  basis_e1[2] = 5.0 / std::sqrt(38.0);
+  basis_e2[0] = 5.0 / std::sqrt(195.0);
+  basis_e2[1] = 7.0 / std::sqrt(195.0);
+  basis_e2[2] = 11.0 / std::sqrt(195.0);
+  basis_pi[0] = 3.0 / std::sqrt(83.0);
+  basis_pi[1] = 5.0 / std::sqrt(83.0);
+  basis_pi[2] = 7.0 / std::sqrt(83.0);
+
+  particle_loop(
+      "one_way_maxwellian_flux_init_loop", particle_group,
+      [=](auto particle_index, auto flow_speed_dat, auto basis_e1_dat,
+          auto basis_e2_dat, auto basis_pi_dat) {
+        for (int i = 0; i < 3; ++i) {
+          flow_speed_dat[i] = flow_speed[i];
+          basis_e1_dat[i] = basis_e1[i];
+          basis_e2_dat[i] = basis_e2[i];
+          basis_pi_dat[i] = basis_pi[i];
+        }
+      },
+      Access::read(ParticleLoopIndex{}),
+      Access::write(Sym<REAL>("FLUID_FLOW_SPEED")),
+      Access::write(Sym<REAL>("SURFACE_BASIS_E1")),
+      Access::write(Sym<REAL>("SURFACE_BASIS_E2")),
+      Access::write(Sym<REAL>("SURFACE_BASIS_PI")))
+      ->execute();
+
+  const REAL norm_ratio = 1.0;
+  const REAL sampled_e1 =
+      (-2.0 * std::sqrt(2.0) + 3.0 * std::sqrt(3.0) + 5.0 * std::sqrt(5.0)) /
+      std::sqrt(38.0);
+  const REAL sampled_e2 =
+      (-5.0 * std::sqrt(2.0) + 7.0 * std::sqrt(3.0) + 11.0 * std::sqrt(5.0)) /
+          std::sqrt(195.0) -
+      std::sqrt(2.0 * norm_ratio) * std::sqrt(-2 * std::log(0.75));
+  const REAL sampled_pi = 3.0 * std::sqrt(2.0 * norm_ratio);
+
+  std::array<REAL, 3> expected_vals;
+
+  for (int i = 0; i < 3; i++) {
+    expected_vals[i] = sampled_e1 * basis_e1[i] + sampled_e2 * basis_e2[i] +
+                       sampled_pi * basis_pi[i];
+  }
+
+  auto rng_lambda = [&]() -> REAL { return 0.75; };
+  auto rng_kernel = host_atomic_block_kernel_rng<REAL>(rng_lambda, 1000);
+
+  OneWayMaxwellianFluxSampler sampler(norm_ratio, rng_kernel,
+                                      get_default_map());
+  auto sampler_on_device = sampler.get_on_device_obj();
+
+  auto req_int_props_ = sampler.get_required_int_sym_vector();
+  auto req_real_props_ = sampler.get_required_real_sym_vector();
+
+  particle_loop(
+      "one_way_maxwellian_flux_sample_loop", particle_group,
+      [=](auto particle_index, auto req_int_props, auto req_real_props,
+          auto sampled_velocity, auto kernel) {
+        auto sampled = sampler_on_device.calc_data(
+            particle_index, req_int_props, req_real_props, kernel);
+        sampled_velocity[0] = sampled[0];
+        sampled_velocity[1] = sampled[1];
+        sampled_velocity[2] = sampled[2];
+      },
+      Access::read(ParticleLoopIndex{}),
+      Access::write(sym_vector<INT>(particle_group, req_int_props_)),
+      Access::read(sym_vector<REAL>(particle_group, req_real_props_)),
+      Access::write(Sym<REAL>("SAMPLED_VELOCITY")),
+      Access::read(sampler.get_rng_kernel()))
+      ->execute();
+
+  const int cell_count = particle_group->domain->mesh->get_cell_count();
+  for (int i = 0; i < cell_count; i++) {
+    auto sampled_cell =
+        particle_group->get_cell(Sym<REAL>("SAMPLED_VELOCITY"), i);
+
+    for (int row = 0; row < sampled_cell->nrow; row++) {
+      EXPECT_NEAR(sampled_cell->at(row, 0), expected_vals[0], 1e-14);
+      EXPECT_NEAR(sampled_cell->at(row, 1), expected_vals[1], 1e-14);
+      EXPECT_NEAR(sampled_cell->at(row, 2), expected_vals[2], 1e-14);
+    }
+  }
+
+  particle_group->sycl_target->free();
+  particle_group->domain->mesh->free();
+}
